22611e64543cac1bc2ef98d03ae254571e63e5b8
Daniel Chen <chend@vt.edu>
2017-06-27 17:49:07 -0400

    add empty lines between comments

--- dkillall-docker_stop_remove_all.sh
+++ dkillall-docker_stop_remove_all.sh
@@ -1,6 +1,12 @@
+echo "docker ps -a:"
+echo ""
 docker ps -a
-
+echo ""
+echo "stopping and removing above containers"
+echo ""
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
-
+echo ""
+echo "docker ps -a:"
+echo ""
 docker ps -a
