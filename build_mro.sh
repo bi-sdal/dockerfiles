d32f3c0167d0ff74c8c5333eb60bd7d37ee8b057
Daniel Chen <chend@vt.edu>
2017-06-27 17:52:34 -0400

    convenience scripts to build mro and all r

---
+++ build_mro.sh
@@ -0,0 +1,11 @@
+cd c7
+dbuildnocache
+cd ..
+
+cd ldap-ssh-c7
+dbuildnocache
+cd ..
+
+cd mro-ldap-ssh-c7
+dbuildnocache
+cd ..
---
+++ build_r.sh
@@ -0,0 +1,20 @@
+cd c7
+dbuildnocache
+cd ..
+
+cd ldap-ssh-c7
+dbuildnocache
+cd ..
+
+cd mro-ldap-ssh-c7
+dbuildnocache
+cd ..
+
+cd rss-mro-ldap-ssh-c7
+dbuildnocache
+cd ..
+
+cd shy-mro-ldap-ssh-c7
+dbuildnocache
+cd ..
+
